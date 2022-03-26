9294eaf53287a35439c2ed7074bfa4c4001f936f
Shen <qazxdrcssc2006@163.com>
2022-03-26 16:07:31 +0800

    Remove placeholder test

---
+++ qnemu/gb/gpu/GbGpu.h
@@ -0,0 +1,17 @@
+/* 
+ *  Copyright [2022] <qazxdrcssc2006@163.com>
+ */
+
+#pragma once
+
+#include "qnemu/gb/GbDeviceInterface.h"
+
+namespace qnemu
+{
+
+class GbGpu : GbDeviceInterface
+{
+
+};
+
+}  // namespace qnemu
