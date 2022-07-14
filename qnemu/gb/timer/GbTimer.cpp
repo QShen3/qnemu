0a6e25f31ff53ca3358fb5a0743d385030c610a4
Shen <qazxdrcssc2006@163.com>
2022-07-15 00:59:20 +0800

    Implement reset of timer

--- qnemu/gb/timer/GbTimer.cpp
+++ qnemu/gb/timer/GbTimer.cpp
@@ -27,7 +27,7 @@ bool GbTimer::accepts(uint16_t address) const
 uint8_t GbTimer::read(uint16_t address) const
 {
     if (address == 0xFF04) {
-        return registers.divider;
+        return registers.divider >> 8;
     }
     if (address == 0xFF05) {
         return registers.timerCounter;
@@ -69,22 +69,52 @@ void GbTimer::step()
 {
     registers.divider++;
 
+    if (overflow) {
+        ticksSinceOverflow++;
+        if (ticksSinceOverflow == 4) {
+            interruptHandler->registers.timerRequest = 1;
+        } else if (ticksSinceOverflow == 5) {
+            registers.timerCounter = registers.timerModulo;
+        } else if (ticksSinceOverflow == 6) {
+            registers.timerCounter = registers.timerModulo;
+            overflow = false;
+            ticksSinceOverflow = 0;
+        }
+    }
+
     if (!registers.timerEnable) {
         return;
     }
+    uint8_t bit = 0;
+    if (registers.inputClockSelect == 0) {
+        bit = (registers.divider & 1024) >> 10;
+    } else if (registers.inputClockSelect == 1) {
+        bit = (registers.divider & 16) >> 4;
+    } else if (registers.inputClockSelect == 2) {
+        bit = (registers.divider & 64) >> 6;
+    } else if (registers.inputClockSelect == 3) {
+        bit = (registers.divider & 256) >> 8;
+    }
 
-    if (registers.timerCounter == 0xFF) {
-        registers.timerCounter = registers.timerModulo;
-        interruptHandler->registers.timerRequest = 1;
+    if (bit != previousBit) {
+        if (registers.timerCounter == 0xFF) {
+            overflow = true;
+        }
+        registers.timerCounter++;
+        previousBit = bit;
     }
 }
 
 void GbTimer::reset()
 {
-    registers.divider = 0xAB;
+    registers.divider = 0xAB00;
     registers.timerCounter = 0;
     registers.timerModulo = 0;
     registers.timerControl = 0xF8;
+
+    overflow = false;
+    previousBit = 0;
+    ticksSinceOverflow = 0;
 }
 
 }  // namespace qnemu
