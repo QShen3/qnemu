e144993e94d5ee4b94994ee63298e10800b0e08d
Shen <qazxdrcssc2006@163.com>
2023-01-28 18:12:21 +0800

    Create test file for sprite attribute table

---
+++ tests/gb/gpu/SpriteAttributeTableTest.cpp
@@ -0,0 +1,17 @@
+/* 
+ *  Copyright [2023] <qazxdrcssc2006@163.com>
+ */
+
+#include <gtest/gtest.h>
+
+#include "qnemu/gb/gpu/SpriteAttributeTable.h"
+
+namespace qnemuTest
+{
+
+TEST(SpriteAttributeTableTest, Accept)
+{
+
+}
+
+}  // namespace qnemuTest
